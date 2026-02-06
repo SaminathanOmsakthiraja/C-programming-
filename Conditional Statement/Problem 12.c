52438fee04818bb2d324fc216392391c3a1c5671
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-02-06 21:29:31 +0530

    Grade Calculation

---
+++ Conditional Statement/Problem 12.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    if (n<=100&&n>=90) {
+        printf("Grade A");
+    }
+    else if (n<90&&n>=75) {
+        printf("Grade B");
+    }
+    else if (n<75&&n>=50) {
+        printf("Grade C");
+    }
+    else if (n<50&&n>=25) {
+        printf("Grade D");
+    }
+}
