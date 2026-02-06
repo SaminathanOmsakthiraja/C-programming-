effdbd04f41937eb815f0149f408663a4f7ea5ea
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-02-06 21:34:33 +0530

    Divisible by 11

---
+++ Conditional Statement/Problem 14.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    if (n%11==0) {
+        printf("Division by 11");
+    }
+    else {
+        printf("Not Divisible by 11");
+    }
+}
