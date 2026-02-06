7ffbb891b4031bd5a9e909472a7f63509bb25208
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-02-06 21:20:22 +0530

    Vote Eligible detecter

---
+++ Conditional Statement/Problem 9.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    if (n>=18) {
+        printf("Eligible to vote");
+    }
+    else {
+        printf("Not Eligible to vote");
+    }
+}
