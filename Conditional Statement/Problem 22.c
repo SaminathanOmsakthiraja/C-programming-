7fdc9f75d0a6404d2dab41e8aecc0768b9c0d8af
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-02-06 22:12:18 +0530

    Days of week

--- Conditional Statement/Problem 22.c
+++ Conditional Statement/Problem 22.c
@@ -5,9 +5,11 @@ int main() {
     switch (n) {
         case 1:
             printf("Sunday");break;
-        case 2:
-            printf("Tuesday");break;
+	    case 2:
+	        printf("Monday");break;
         case 3:
+            printf("Tuesday");break;
+        case 4:
             printf("Wednesday"); break;
         case 5:
             printf("Thursday"); break;
