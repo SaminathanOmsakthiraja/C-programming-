8bdce6b87f08adb10a99912a70bca25708036332
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-02-07 09:44:02 +0530

    Exam Attendance Eligibility

---
+++ Conditional Statement Advance/Problem 13.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    if (n>=75) {
+        printf("Eligible for Exam");
+    }
+    else {
+        printf("Not Eligible");
+    }
+}
