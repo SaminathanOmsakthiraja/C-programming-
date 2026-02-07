a053d7af46e94db1618ab26ed0ac1e0d2f4b32b5
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-02-07 09:22:24 +0530

    Password Length Check

---
+++ Conditional Statement Advance/Problem 2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    if (n>=8) {
+        printf("Strong Password");
+    }
+    else {
+        printf("Weak Password");
+    }
+}
