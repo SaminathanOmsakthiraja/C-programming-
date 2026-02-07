53044571ce9154fe405b9819da6c89ea58f05a67
SaminathanOmsakthiraja <omsaminathan@gmail.com>
2026-02-07 09:24:42 +0530

    Online Order Discount

---
+++ Conditional Statement Advance/Problem 3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    if (n>=1000) {
+        printf("Discount Applied");
+    }
+    else {
+        printf("No Discount");
+    }
+}
